6434a80cd6e745ce86a48b2f65c940a259e347a0
Ali Talal <ali549954@gmail.com>
2026-03-22 17:10:34 +0100

    Step 4: Add AngularJS frontend (script.js and index.html)

---
+++ Books/public/script.js
@@ -0,0 +1,40 @@
+angular.module('myApp', [])
+  .controller('myCtrl', function($scope, $http) {
+    function fetchBooks() {
+      $http.get('/book')
+        .then(response => {
+          $scope.books = response.data;
+        })
+        .catch(error => {
+          console.error('Error fetching books:', error);
+        });
+    }
+    fetchBooks();
+
+    $scope.del_book = function(book) {
+      $http.delete(`/book/${book.isbn}`)
+        .then(() => {
+          fetchBooks();
+        })
+        .catch(error => {
+          console.error('Error deleting book:', error);
+        });
+    };
+
+    $scope.add_book = function() {
+      const newBook = {
+        name: $scope.Name,
+        isbn: $scope.Isbn,
+        author: $scope.Author,
+        pages: $scope.Pages
+      };
+      $http.post('/book', newBook)
+        .then(() => {
+          fetchBooks();
+          $scope.Name = $scope.Isbn = $scope.Author = $scope.Pages = '';
+        })
+        .catch(error => {
+          console.error('Error adding book:', error);
+        });
+    };
+  });
